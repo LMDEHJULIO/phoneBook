d60bd49c30f03e06921c3d30b1d579e8db1a2a0e
Julio Rodriguez <juliodevelops@gmail.com>
2023-09-19 12:52:13 -0400

    Initial commit

---
+++ src/main/java/org/example/PhoneBook.java
@@ -0,0 +1,64 @@
+package org.example;
+
+public class PhoneBook {
+    /**
+     * @description Phonebook class that maps a name to phoneNumbers
+     */
+//
+//     initalize private array/arrayList of names
+//    initialize private array/arrayList of phoneNumbers
+
+
+    // initialize Map<Person, PhoneNumber> or
+    // Map<String, String[]>
+
+
+    /**
+     * addAll - adds many phone numbers to a single name entry
+     * @params - phoneNumbers to be added to
+     * @description Phonbook class that maps a name to phoneNumbers
+     */
+
+    // For each phoneNumber - put phoneNumber as value @ Person Key
+    // probably establish some error checking code to ensure phone has all digits, is not null, etc
+    // if so, put to key
+
+    /**
+     * remove - removes an entry to the composite associated data type
+     * @param key that is set to have its value removed
+     * @description Phonbook class that maps a name to phoneNumbers
+     */
+
+    //Map.remove at the given key
+
+    /**
+     * hasEntry
+     * remove - removes an entry to the composite associated data type
+     * @description Phonbook class that maps a name to phoneNumbers
+     */
+
+    /**
+     * lookUp
+     * remove - removes an entry to the composite associated data type
+     * @description Phonbook class that maps a name to phoneNumbers
+     */
+
+    /**
+     * reverseLookUp
+     * remove - removes an entry to the composite associated data type
+     * @description Phonbook class that maps a name to phoneNumbers
+     */
+
+    /**
+     * getAllContactNames
+     * @params
+     * @return
+     * remove - removes an entry to the composite associated data type
+     * @description Phonbook class that maps a name to phoneNumbers
+     */
+
+
+
+
+
+}
