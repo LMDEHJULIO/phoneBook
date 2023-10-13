f358e70acc54c6cc0d1d22dc32c8a6493f459151
Julio Rodriguez <juliodevelops@gmail.com>
2023-10-13 10:57:27 -0400

    Add working PB tests

--- src/test/java/org/example/PBUtilityTest.java
+++ src/test/java/org/example/PBUtilityTest.java
@@ -15,4 +15,23 @@ class PBUtilityTest {
     void isNull(){
         assertThrows(IllegalArgumentException.class, () -> PBUtility.checkIfNull(null, "Expected exception thrown"));
     }
+
+    @Test
+    public void testValidNumber(){
+        assertFalse(PBUtility.isValidPhoneNumber("121312"));
+        assertFalse(PBUtility.isValidPhoneNumber("asd"));
+    }
+
+    @Test void testValidInput(){
+        PBUtility.validateInput("Steve", "18001231234");
+    }
+//
+//    @Test void testInvalidInput(){
+//        assertThrows(IllegalArgumentException.class, () -> PBUtility.validateInput("Steve", "1233"));
+//
+//    }
+
+    @Test void testDups(){
+        assertThrows(IllegalArgumentException.class, () -> PBUtility.validateInput("Steve", "18001231234", "18001231234"));
+    }
 }
